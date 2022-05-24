d6fd54f6484beca0088ef58e32a711312159c7f9
Do Anh Tu <anhtu.do1998@gmail.com>
2022-05-24 21:51:46 +0800

    Edit script to deploy to rinkeby

--- lottery-contract/scripts/deploy_NFTcontract.py
+++ lottery-contract/scripts/deploy_NFTcontract.py
@@ -5,9 +5,16 @@ from scripts.createMetadata import createMetadata, upload_to_ipfs
 
 def main():
     account = get_account()
+    # Chainlink subscription ID
+    subscription_id = 4430
+    coordinator_addr = "0x6168499c0cFfCaCD319c818142124B7A15E857ab"
+    keyhash = "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc"
+    # In Unix Epoch timestamp
+    ballot_date = 1653399369
     print(network.show_active())
-    NFT_Collection = NFTCollection.deploy(
+    NFT_Raffle = Raffle.deploy(
+        subscription_id, coordinator_addr, keyhash, ballot_date,
         {"from": account},
         publish_source=True,
     )
-    return NFT_Collection
+    return Raffle
